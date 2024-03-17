07297a4d9759bd01436644b32127f65bc5bfe894
Levi Gleik <leviacedo1@gmail.com>
2024-03-16 21:29:14 -0300

    :recycle: refactor: optimizing and splitting code

--- src/app/(private)/(dashboard)/page.tsx
+++ src/app/(private)/(dashboard)/page.tsx
@@ -1,17 +1,10 @@
 import { format } from 'date-fns'
-import {
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Divider,
-} from '@nextui-org/react'
-import { FaFilter } from 'react-icons/fa'
-import { CalendarDashboard } from '@/app/(private)/(dashboard)/components/calendar.dashboard'
-import { ModalDashboard } from '@/app/(private)/(dashboard)/components/modal'
-import { months } from '@/app/(private)/(dashboard)/constants'
-import { CardDashboard } from '@/app/(private)/(dashboard)/components/card.dashboard'
+
+import { Calendar } from './components/months/calendar'
+import { ModalVacationDashboard } from './components/months/modal'
+import { months } from './constants'
+import { ModalFilterDashboard } from './components/summary/modal'
+import { SummaryDashboard } from '@/app/(private)/(dashboard)/components/summary'
 
 export default function Home() {
   return (
@@ -29,25 +22,14 @@ export default function Home() {
               <h2 className="text-center text-2xl font-bold">
                 {format(new Date(2024, month, 0), 'MMMM')}
               </h2>
-              <CalendarDashboard month={month} />
+              <Calendar month={month} />
             </div>
           ))}
         </div>
       </div>
-      <ModalDashboard />
-      <div className="flex w-full flex-col items-center justify-center gap-4 px-4 pt-6">
-        <div className="flex w-full justify-between pl-4">
-          <h1 className="text-2xl font-bold">Summary of the year</h1>
-          <Button isIconOnly variant="light" className="w-fit rounded-full">
-            <FaFilter className="text-main" />
-          </Button>
-        </div>
-        {months.map((month) => (
-          <div key={month} className="min-h-[100px] w-full">
-            <CardDashboard month={month} />
-          </div>
-        ))}
-      </div>
+      <ModalVacationDashboard />
+      <ModalFilterDashboard />
+      <SummaryDashboard />
     </div>
   )
 }
