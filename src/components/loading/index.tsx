d348fe71477007a1e15abfaa798374a5b1df307e
Levi Gleik <leviacedo1@gmail.com>
2024-03-17 19:30:04 -0300

    :lipstick: feat: changed h-full to 100dvh in Loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-[100dvh] w-full items-center justify-center">
       <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-primary"></div>
     </div>
   )
